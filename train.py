31c68a3b0218e5606a9fb1a52824a8ddb7fba3f6
jim-ndmisha <94824818+jim-ndmisha@users.noreply.github.com>
2023-05-28 19:22:16 +0800

    Update train.py

--- train.py
+++ train.py
@@ -156,7 +156,7 @@ if __name__ == "__main__":
     #   UnFreeze_Epoch          模型总共训练的epoch
     #   Unfreeze_batch_size     模型在解冻后的batch_size
     #------------------------------------------------------------------#
-    UnFreeze_Epoch      = 200
+    UnFreeze_Epoch      = 300
     Unfreeze_batch_size = 2
     #------------------------------------------------------------------#
     #   Freeze_Train    是否进行冻结训练
